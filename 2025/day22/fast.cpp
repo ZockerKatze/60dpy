164e8196cfeb9d0f7abe66e70e31ccc1f887a231
Rattatwinko <89251490+ZockerKatze@users.noreply.github.com>
2025-02-25 09:17:11 +0000

    update c++
    + smoll update to c++

--- 2025/day22/fast.cpp
+++ 2025/day22/fast.cpp
@@ -1,23 +1,3 @@
-#include <iostream>
-
-using namespace std;
-
-class Solution
-{
-    public:
-    bool check(int x)
-    {
-        if(x%3==0 && x%5==0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-};
-
-int main()
-{
-    Solution sol;
-    cout << boolalpha << sol.check(15) << endl;
-    return 0;
-}
+#include <iostream> 
+class Solution { public: bool check(int x){ if(x%3==0 && x%5==0) { return true;} else { return false; }}}; 
+int main() { Solution sol; std::cout << std::boolalpha << sol.check(15) << std::endl;}
